991ba306637e83a9ebc84d62f853422762ae8f3a
Vivan <vivanpamta221@gmail.com>
2026-03-10 11:49:20 +0530

    Initial commit: React Counter App with styling

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>React Counter Application</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
